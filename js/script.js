fc0dfcd7fcc816a7d612df8d2f671ce38145bd83
JobConnect777 <allulia041241@gmail.com>
2026-07-11 01:46:09 +0700

    Add event listeners to category buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const buttons = document.querySelectorAll(".category-btn");
 
+buttons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+const menu = button.nextElementSibling;
+
+menu.classList.toggle("active");
+
+});
+
+});
